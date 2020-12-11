b9c1ad17a47a43c90e9e689835caeddb2c72cc46
Neville Grech <nevillegrech@gmail.com>
2020-12-11 15:31:28 +0100

    improve interactive mode

--- process_analysis_results.py
+++ process_analysis_results.py
@@ -8,6 +8,8 @@ from collections import defaultdict
 from os.path import abspath, dirname, join, getsize
 
 BENCHMARKS_DIR = dirname(abspath(__file__))
+INTERACTIVE = True
+assert(__name__ == '__main__', "Not a library")
 
 class Evaluator:
     def __init__(self, filepath):
@@ -34,7 +36,11 @@ class GigahorseEvaluator(Evaluator):
 
         for label in self.labels:
             example = label['bytecode-path']
-            example_len = len(open(join(join(BENCHMARKS_DIR, 'vulnerable-bytecode'), example)).read())
+            try:
+                program_size = len(open(join(join(BENCHMARKS_DIR, 'vulnerable-bytecode'), example)).read())
+            except IOError:
+                # not found
+                program_size = 0
             expected_vulnerability_type = None
             for v in self.mappings.values():
                 if example.startswith(v):
@@ -47,13 +53,13 @@ class GigahorseEvaluator(Evaluator):
                 if v == expected_vulnerability_type:
                     true_positives |=  is_vuln
                     if is_vuln:
-                        results_per_vuln.add((example, k, 0, 1, example_len))
+                        results_per_vuln.add((example, k, 0, 1, program_size))
                 else:
                     false_positives += int(is_vuln)
                     if is_vuln:
-                        results_per_vuln.add((example, k, 1, 0, example_len))
+                        results_per_vuln.add((example, k, 1, 0, program_size))
     
-            results.append((example, expected_vulnerability_type, int(true_positives), false_positives, example_len))
+            results.append((example, expected_vulnerability_type, int(true_positives), false_positives, program_size))
         return results, results_per_vuln
 
 
@@ -86,40 +92,53 @@ class SymvalicEvaluator(GigahorseEvaluator):
 
 evaluator = eval(sys.argv[1]+'Evaluator')(sys.argv[2])
 
-results, results_per_vuln = evaluator.process_results()
+flagged_by_oracle, flagged_by_tool = evaluator.process_results()
 
 # construct dataframe
-results_df = DataFrame({
-    'expected_vulnerability_type' : {i: v for i, v, tp, fp, _ in results},
-    'tp' : {i: tp for i, v, tp, fp, _ in results},
-    'fp' : {i: fp for i, v, tp, fp, _ in results},
-    'fn' : {i: int(tp == 0) for i, v, tp, fp, _ in results},
-    'total_v' : {i: 1 for i, v, tp, fp, _ in results},
-    'example_len' : {i: e for i, v, tp, fp, e in results}
+flagged_by_oracle = DataFrame({
+    'expected_vulnerability_type' : {i: v for i, v, tp, fp, _ in flagged_by_oracle},
+    'tp' : {i: tp for i, v, tp, fp, _ in flagged_by_oracle},
+    'fp' : {i: fp for i, v, tp, fp, _ in flagged_by_oracle},
+    'fn' : {i: int(tp == 0) for i, v, tp, fp, _ in flagged_by_oracle},
+    'total_v' : {i: 1 for i, v, tp, fp, _ in flagged_by_oracle},
+    'program_size' : {i: e for i, v, tp, fp, e in flagged_by_oracle}
 })
 
-results_per_vuln_df = DataFrame({
-    'example': {i: e for i, (e,v,fp,tp,_) in enumerate(results_per_vuln)},
-    'vulnerability': {i: v for i, (e,v,fp,tp,_) in enumerate(results_per_vuln)},
-    'tp': {i: tp for i, (e,v,fp,tp,_) in enumerate(results_per_vuln)},
-    'fp': {i: fp for i, (e,v,fp,tp,_) in enumerate(results_per_vuln)},
-    'example_len': {i: example_len for i, (e,v,_,_,example_len) in enumerate(results_per_vuln)},
+flagged_by_tool = DataFrame({
+    'example': {i: e for i, (e,v,fp,tp,_) in enumerate(flagged_by_tool)},
+    'vulnerability': {i: v for i, (e,v,fp,tp,_) in enumerate(flagged_by_tool)},
+    'tp': {i: tp for i, (e,v,fp,tp,_) in enumerate(flagged_by_tool)},
+    'fp': {i: fp for i, (e,v,fp,tp,_) in enumerate(flagged_by_tool)},
+    'program_size': {i: program_size for i, (e,v,_,_,program_size) in enumerate(flagged_by_tool)},
 })
 
-results_per_vuln_grouped = results_per_vuln_df.groupby('vulnerability').sum()
-results_per_vuln_grouped['tp/fp'] = results_per_vuln_grouped.tp / results_per_vuln_grouped.fp
+flagged_by_tool_grouped = flagged_by_tool.groupby('vulnerability').sum()
+flagged_by_tool_grouped['tp/fp'] = flagged_by_tool_grouped.tp / flagged_by_tool_grouped.fp
 
-results_grouped = results_df.groupby('expected_vulnerability_type').sum()
+flagged_by_oracle_grouped = flagged_by_oracle.groupby('expected_vulnerability_type').sum()
 
 def compute_pr(df):
     df['precision'] = df.tp / (df.tp + df.fp)
     df['recall'] = df.tp / (df.tp + df.fn)
     df['F1'] = df.precision * df.recall / (df.precision + df.recall)
 
-compute_pr(results_grouped)
+compute_pr(flagged_by_oracle_grouped)
 
-print(results_grouped)
+print(flagged_by_oracle_grouped)
 print()
-print(results_per_vuln_grouped.sort_values('tp/fp'))
+print(flagged_by_tool_grouped.sort_values('tp/fp'))
 
-import pdb; pdb.set_trace()
+if INTERACTIVE:
+    descriptions = {
+        'flagged_by_oracle': "Useful for examining false negatives for a particular tool, and plotting results in a paper.",
+        'flagged_by_tool': "Useful for finding which analyses produce the most false positives for a particular tool",
+        'flagged_by_oracle_grouped': "As above, but grouped by vulnerability",
+        'flagged_by_tool_grouped': "As above, but grouped by vulnerability",
+       }
+    print('Entering interactive mode, you can query the following Pandas dataframes using the PDB shell')
+    print('-'*80)
+    for k, v in descriptions.items():
+        assert k in globals()
+        print(f'{k}: {v}')
+    
+    import pdb; pdb.set_trace()

